0a181a75e33ee393a2898ba92b58335646dbdf71
rdelvallej32 <rdelvallej@gmail.com>
2016-05-12 11:28:21 -0400

    Add Promise for async read
    Able to readfile, but no upload yet

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -5,12 +5,23 @@ const fs = require('fs');
 //if a path is not provided pass a string. Its for testing
 let filename = process.argv[2] || '';
 
-fs.readFile(filename, {encoding: 'utf8'}, (err, data) => {
-  if (err) {
-    //log the error to see if it is an error
-    return console.error(err);
-  }
+const readFile = (filename) =>
+  new Promise((resolve, reject) => {
 
-  //success will log the filename along with the data length bytes
-  console.log(`${filename} is ${data.length} bytes long`);
-});
+    fs.readFile(filename, {encoding: 'utf8'}, (err, data) => {
+      if (err) {
+        //log the error to see if it is an error
+        reject(err);
+      } else {
+        //pass that data in to resolve
+        resolve(data);
+      }
+    });
+  });
+
+  readFile(filename)
+  .then((data) => console.log(`${filename} is ${data.length} bytes long`))
+  .catch(console.error);
+
+
+//success will log the filename along with the data length bytes
