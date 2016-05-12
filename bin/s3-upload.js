09fe3f689a06642ab1ddadd1038dcbc6d63a3117
rdelvallej32 <rdelvallej@gmail.com>
2016-05-12 15:29:19 -0400

    Move S3 specifics to separate file
    Refactor so AWS only code is in lib

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -10,14 +10,8 @@ let filename = process.argv[2] || '';
 ///file-type returns an object that returns ext and mime: npm module/node
 const fileType = require('file-type');
 
-///crypto Generates cryptographically strong pseudo-random data: npm module/node
-const crypto = require('crypto');
-
-//require AWS
-const AWS = require('aws-sdk');
-
-//constructor that makes a new AWS to upload does
-const s3 = new AWS.S3();
+//require the aws upload function
+const awsS3Upload = require('../lib/aws-s3-upload.js');
 
 ///creation of the mimetype which returns an object with ext and mime
 //always return object with extension and mime.
@@ -44,52 +38,6 @@ const readFile = (filename) =>
     });
   });
 
-  const randomHexString = (length) =>
-    new Promise((resolve, reject) => {
-      crypto.randomBytes(length, (err, buf) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(buf.toString('hex'));
-        }
-      });
-    });
-
-//this is the function that uploads a file
-  const awsUpload = (file) =>
-
-    randomHexString(16)
-    .then((filename) => {
-      //sets directory to current date
-      let dir = new Date().toISOString().split('T')[0];
-      ///creates aws params
-      return {
-        ACL: 'public-read',
-        ///the body is the actual data that is being streamed
-        Body: file.data,
-        Bucket: process.env.AWS_S3_BUCKET_NAME,
-        ///what type of file it is
-        ContentType: file.mime,
-        ///path that gets set up, make sure you are not using a path that already exists
-        ///because it will overwrite it!
-        ////add file ext to the string from filetype method
-        /// add date as the beginning for directory to name by the date it was uploaded
-        Key: `${dir}/${filename}.${file.ext}`,
-      };
-
-    }).then(params =>
-       new Promise((resolve, reject) => {
-        s3.upload(params, (err, data) => {
-          if(err) {
-            reject(err);
-          } else {
-            resolve(data);
-          }
-        });
-      })
-    );
-
-
 ///readfile returns a file and then passes the data, which get turned into
 ///
   readFile(filename)
@@ -98,7 +46,7 @@ const readFile = (filename) =>
     file.data = data;
     return file;
   })
-  .then(awsUpload)
+  .then(awsS3Upload)
   ///
   .then((s3response) => console.log(s3response))
   ///handles the error
